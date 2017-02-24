9212c7038b567360fc818bba5d3eb0aeb144d67d
Vincent Cueille <vcueille@mcai.fr>
2017-02-24 09:12:11 +0100

    Déplacement des scripts de recherche de pays et villes dans le fichier js externe main.js

---
+++ scripts/main.js
@@ -0,0 +1,77 @@
+
+var geocoder;
+var map;
+// initialisation de la carte Google Map de départ
+function initialiserCarte() 
+{
+  geocoder = new google.maps.Geocoder();
+  // On affiche tout le planisfère
+  var latlng = new google.maps.LatLng(52.629729, -1.131592);
+  var mapOptions = {
+    zoom      : 2,
+    center    : latlng,
+    mapTypeId : google.maps.MapTypeId.ROADMAP
+  }
+  // map-canvas est le conteneur HTML de la carte Google Map
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+}
+
+$(document).ready(function() 
+{
+	geocoder = new google.maps.Geocoder();
+//Initialisation de Google Maps
+  var mapOptions = 
+  {
+    zoom: 3,
+    center: new google.maps.LatLng(52.629729, -1.131592)
+  };
+
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+});
+function TrouverPays() {
+  // Récupération de l'adresse tapée dans le formulaire
+  var pays = document.getElementById('pays').value;
+  geocoder.geocode( { 'address': pays}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      // Récupération des coordonnées GPS du lieu tapé dans le formulaire
+      var strposition = results[0].geometry.location+"";
+      strposition=strposition.replace('(', '');
+      strposition=strposition.replace(')', '');
+      // Affichage des coordonnées dans le <span>
+      //document.getElementById('text_latlng').innerHTML='Coordonnées : '+strposition;
+      // Création du marqueur du lieu (épingle)
+      var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+      });
+    } else {
+      alert('Adresse introuvable: ' + status);
+    }
+  });
+}
+function TrouverVille() {
+  // Récupération de l'adresse tapée dans le formulaire
+  var ville = document.getElementById('ville').value;
+  geocoder.geocode( { 'address': ville}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      // Récupération des coordonnées GPS du lieu tapé dans le formulaire
+      var strposition = results[0].geometry.location+"";
+      strposition=strposition.replace('(', '');
+      strposition=strposition.replace(')', '');
+      // Affichage des coordonnées dans le <span>
+      //document.getElementById('text_latlng').innerHTML='Coordonnées : '+strposition;
+      // Création du marqueur du lieu (épingle)
+      var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+      });
+    } else {
+      alert('Adresse introuvable: ' + status);
+    }
+  });
+}
+// Lancement de la construction de la carte google map
+google.maps.event.addDomListener(window, 'load', initialiserCarte);
