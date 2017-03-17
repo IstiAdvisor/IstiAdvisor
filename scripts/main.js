4c996583611017ba59fb9ea89abd8b14903e79e9
Vincent Cueille <vcueille@mcai.fr>
2017-03-17 20:39:23 +0100

    Modifications faites par Christophe, Cindy et Simon, concernant l'affichage d'informations

--- scripts/main.js
+++ scripts/main.js
@@ -63,3 +63,66 @@ function TrouverVille() {
 }
 // Lancement de la construction de la carte google map
 google.maps.event.addDomListener(window, 'load', initialiserCarte);
+
+function DescriptionPays()
+{
+	// Déclaration de variables
+	var documentsAavoir=null;
+	var langue=null;
+	var monnaie=null;
+	var particularite=null;
+	var pays=null;
+	var liDocument=null;
+	var liLangue=null;
+	var liMonnaie=null;
+	var liParticularite=null
+	var br=null;
+	var liBr=null;
+	
+	// Suppression d'éléments déjà existant
+	// Declaration des variables
+	var Li = null;
+	var Element = null;
+
+	// creation de plusieurs lignes <li>
+	pays = document.getElementById("pays").value;	// recuperation du nom de la tache saisie dans la barre
+	documentsAavoir = document.createElement("li");	// creation d'un élément de type li
+	langue = document.createElement("li");	// creation d'un élément de type li
+	monnaie = document.createElement("li");	// creation d'un élément de type li
+	particularite = document.createElement("li");	// creation d'un élément de type li
+	
+	//Vérification du pays
+	if(pays=="Nouvelle-Zélande")
+	{
+		documentsAavoir=document.createTextNode("Documents à avoir: Passeport, VISA si durée > 3 mois ");	// texte 
+		langue=document.createTextNode("Langue utilisée: anglais ");	// texte 
+		monnaie=document.createTextNode("Monnaie utilisée: dolar néo-zélandais [NZD] ");	// texte 
+		particularite=document.createTextNode("Particularité: fan de rugby, ALL-BLACKS ");	// texte 
+		
+		liDocument = document.getElementById("description-pays").appendChild(documentsAavoir);	// insertion de nouvel élément "li" dans "description-pays"
+		//liBr=document.getElementById("description-pays").appendChild(br); // saut de ligne
+		liLangue = document.getElementById("description-pays").appendChild(langue);
+		//liBr=document.getElementById("description-pays").appendChild(br); // saut de ligne
+		liMonnaie = document.getElementById("description-pays").appendChild(monnaie);
+		//liBr=document.getElementById("description-pays").appendChild(br); // saut de ligne
+		liParticularite = document.getElementById("description-pays").appendChild(particularite);
+		//liBr=document.getElementById("description-pays").appendChild(br); // saut de ligne
+	}
+	if(pays=="Brésil")
+	{
+		documentsAavoir=document.createTextNode("Documents à avoir: Passeport, VISA si durée > 3 mois. ");	// texte 
+		langue=document.createTextNode("Langue utilisée: portugais. ");	// texte 
+		monnaie=document.createTextNode("Monnaie utilisée: Réal brésilien [BRL/R$]. ");	// texte 
+		particularite=document.createTextNode("Particularité: fan de foot. ");	// texte 
+		
+		liDocument = document.getElementById("description-pays").appendChild(documentsAavoir);	// insertion de nouvel élément "li" dans "description-pays"
+		liBr=document.getElementById("description-pays").appendChild(br); // saut de ligne
+		liLangue = document.getElementById("description-pays").appendChild(langue);
+		liBr=document.getElementById("description-pays").appendChild(br); // saut de ligne
+		liMonnaie = document.getElementById("description-pays").appendChild(monnaie);
+		liBr=document.getElementById("description-pays").appendChild(br); // saut de ligne
+		liParticularite = document.getElementById("description-pays").appendChild(particularite);
+		liBr=document.getElementById("description-pays").appendChild(br); // saut de ligne
+	}
+
+}
